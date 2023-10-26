b470b656f841f55da49ef46a211ed2bc5c65908a
JimBaba <aalberts.daniel@gmail.com>
2023-10-26 16:52:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const app = express()
+const port = 3000
+
+main().catch(e => console.log(e))
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
+        .then(() => {
+            console.log("Mongoose Connection Established")
+        })
+        .catch((e) => {
+            console.log("Mongoose Connection Error!!", e)
+        })
+}
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
